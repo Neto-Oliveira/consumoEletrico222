50ed210a0ac4de6235fc7dbcbbda97567d44ff8a
Neto Oliveira <92645944+Neto-Oliveira@users.noreply.github.com>
2022-09-25 19:32:50 -0300

    Add files via upload

---
+++ calculo.js
@@ -0,0 +1,28 @@
+var potencia = 0
+var dias = 0
+var hora = 0
+const precoKwh = 0.75
+var consumoMensal = 0.0
+
+
+
+function calcularConsumo(){
+ var potenciaWattsjs = document.getElementById('potenciaWatts')
+ var diasjs = document.getElementById('dias')
+ var horasjs = document.getElementById('horas')
+ var consumoMensal = document.getElementById('consumo')
+
+
+
+ // Potência em Watts x Dias por mês x Horas por dia  / 1000 x Preço kWh
+// 5W x 30 Dias x 8.0 Horas  / 1000x R$ 0.75
+//= R$ 0.90
+ if (potenciaWattsjs > 1) {
+  consumoMensal.innerHTML = ((potenciaWattsjs * diasjs * horasjs) / 1000) * 0.75
+ }
+
+
+}
+
+
+
